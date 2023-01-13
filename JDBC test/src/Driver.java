e1b0480503c1567c6d92b0085003392b2e3e5487
wun-yu-lin <t928394558@gmail.com>
2023-01-14 01:08:26 +0800

    JDBC driver connection

--- JDBC test/src/Driver.java
+++ JDBC test/src/Driver.java
@@ -1,23 +1,63 @@
+import javax.swing.*;
+import javax.swing.plaf.nimbus.State;
+import java.awt.*;
 import java.sql.*;
 
 public class Driver {
-    public static void main(String[] args) {
+    private static Connection connection;
+    public static void main(String[] args) throws SQLException {
+        //main
+        initializedDB();
+        String price = JOptionPane.showInputDialog("Find a video by price, please input integer");
+        String sql_statement = "select * from video where price= ? ;";
+        PreparedStatement pps = connection.prepareStatement(sql_statement); //avoid sql injection
+        pps.setString(1,price);
+        try{
+            ResultSet resultSet = pps.executeQuery();
+            while (resultSet.next()){
+                String videoName = resultSet.getString("videoName");
+                JOptionPane.showMessageDialog(null, videoName);
+            }
+        } catch (SQLException e) {
+            System.out.println("Search SQL is failed");
+            throw new RuntimeException(e);
+        } catch (HeadlessException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        closeDB();
+
+    }
+
+    public static void initializedDB() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
         try {
 
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/winniedatabase","root", "password");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/winniedatabase","root", "password");
             System.out.println("JDBC connection success");
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from video" );
-            while (resultSet.next()){
-                System.out.println(resultSet.getString("videoName"));
-            }
+//            String sql_statement = "select * from video";
+//            Statement statement = connection.createStatement();
+//            ResultSet resultSet = statement.executeQuery(sql_statement);
+//            while (resultSet.next()){
+//                System.out.println(resultSet.getString("videoName"));
+//            }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+    public static void closeDB(){
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 }
